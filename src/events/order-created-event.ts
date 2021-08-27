b550c03f3f04932ae117e328c010c931455100ae
edumueller <eduardo.nicastro@icloud.com>
2021-08-27 00:05:18 -0300

    add order created event

---
+++ src/events/order-created-event.ts
@@ -0,0 +1,10 @@
+import { Subjects } from "./subjects";
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+    userId: string;
+  };
+}
